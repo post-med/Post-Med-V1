05784c3b52c633df34cdf798d167d48cd8b46b88
aniruddh-jain <ani@blockseed.co>
2019-07-09 12:03:44 +0200

    charts color changed

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,46 @@ router.get("/partial", (req, res, next) => {
 });
 
 router.get("/summary", (req, res, next) => {
-  res.render("overview", { arr: [0, 1, 2, 3, 4, 5] });
+  res.render("overview", {
+    arr: [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30,
+      31,
+      32,
+      33,
+      34,
+      35
+    ]
+  });
 });
 
 module.exports = router;
