db140193f7e3efe066186b3eed34970294bd4585
aniruddh-jain <ani@blockseed.co>
2019-07-09 14:56:43 +0200

    background and sidepanel

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,5 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("IronGenerator JS imported successfully!");
+  },
+  false
+);
 
-  console.log('IronGenerator JS imported successfully!');
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "350px";
+}
 
-}, false);
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
